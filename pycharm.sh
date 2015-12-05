c5eac74d28a097cdb0aff4d5ffeab6fd37fc277f
Ron Kurr <kurr@kurron.org>
2015-12-05 20:38:26 +0000

    IDE comes up

--- pycharm.sh
+++ pycharm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 CMD="docker run \
-       --name intellij \
+       --name pychamr \
        --net "host" \
        --env DISPLAY=unix$DISPLAY \
        --user 1000:1000 \
        --volume /tmp/.X11-unix:/tmp/.X11-unix \
        --volume $HOME:/home/developer \
-       kurron/docker-intellij:latest"
+       kurron/docker-pycharm:latest"
 
 echo $CMD
 $CMD
